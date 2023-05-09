4131d06dea6d20f81dfeaa68a783c63196acbfc4
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-09 15:19:24 +0300

    Create schema_based_on_diagram.sql

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,45 @@
+CREATE TABLE patients (
+  id INT,
+  name VARCHAR,
+  date_of_birth DATE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE medical_histories (
+  id INT,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR,
+  PRIMARY KEY (id),
+  FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
+
+CREATE TABLE treatments (
+  id INT,
+  type VARCHAR,
+  name VARCHAR,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoices (
+  id INT,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history__id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (medical_history__id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+  id INT,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT,
+  treatment_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
